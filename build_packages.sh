bf8545c975365e725e15e37d2c927e26acc37830
François Kooman <fkooman@tuxed.net>
2020-02-14 09:43:34 +0100

    add php-saml-sp and php-saml-sp-arwork-eduVPN packages

--- build_packages.sh
+++ build_packages.sh
@@ -16,7 +16,8 @@ PACKAGE_LIST=(\
     vpn-daemon \
     php-fkooman-secookie \
     php-fkooman-saml-sp \
-#    php-saml-sp \
+    php-saml-sp \
+    php-saml-sp-artwork-eduVPN \
     php-fkooman-jwt \
     php-fkooman-otp-verifier \
     php-fkooman-oauth2-server \
